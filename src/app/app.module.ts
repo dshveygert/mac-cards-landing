01219aed1c5d500f488707313e25ea4c654a250a
dshveigert <5706601+dshveigert@users.noreply.github.com>
2022-06-02 19:02:54 +0700

    Remove redundant router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +12,6 @@ import { ApiModule } from './api/api.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     RoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
