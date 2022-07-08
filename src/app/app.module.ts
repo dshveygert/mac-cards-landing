ceea339cf5bf8f69e83b830c2ce05fe501472e04
dshveigert <5706601+dshveigert@users.noreply.github.com>
2022-07-08 13:36:44 +0700

    Changes in routing for future prerendering settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,55 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
-import { AppComponent } from './app.component';
-import { RoutingModule } from './routing/routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
-import { ApiModule } from './api/api.module';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {environment} from 'src/environments/environment';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule} from 'ngx-google-analytics';
+import {ApiModule} from './api/api.module';
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
+import {SharedModule} from "./shared/shared.module";
+import {LayoutComponent} from "./routing/components/layout/layout.component";
+import {NavigationComponent} from "./routing/components/navigation/navigation.component";
+import {HomePageComponent} from "./home/pages/home-page/home-page.component";
+import {FooterComponent} from "./routing/components/footer/footer.component";
+import {NotFoundPage} from "./routing/components/not-found/not-found.page";
+
+const components = [AppComponent, LayoutComponent, NavigationComponent, HomePageComponent, FooterComponent];
+
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: 'payment', loadChildren: () => import(`./payment/payment.module`).then(m => m.PaymentModule)},
+      {
+        path: 'consultation',
+        loadChildren: () => import(`./consultation/consultation.module`).then(m => m.ConsultationModule)
+      },
+      {path: 'docs', loadChildren: () => import(`./documents/documents.module`).then(m => m.DocumentsModule)},
+      {path: '', component: HomePageComponent, pathMatch: 'full'},
+      {path: '**', component: NotFoundPage}
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: components,
   imports: [
-    BrowserModule,
-    RoutingModule,
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'disabled',
+      anchorScrolling: 'enabled',
+      preloadingStrategy: PreloadAllModules,
+      initialNavigation: 'enabledBlocking'
+    }),
     HttpClientModule,
     BrowserAnimationsModule,
     NgxGoogleAnalyticsModule.forRoot(environment.analytics.ga),
     NgxGoogleAnalyticsRouterModule,
-    ApiModule
+    ApiModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
